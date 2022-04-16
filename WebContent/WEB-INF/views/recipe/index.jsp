1aa328845b8bd797fc127ffef5506962efa701b0
Reihold <Reihold@192.168.1.5>
2022-04-16 17:33:39 +0900

    制作レシピ一覧を作成

--- WebContent/WEB-INF/views/recipe/index.jsp
+++ WebContent/WEB-INF/views/recipe/index.jsp
@@ -13,6 +13,36 @@
 
         <h2>レシピアプリケーション（仮）</h2>
 
+        <ul>
+            <c:forEach var = "recipe" items = "${recipes }" >
+                <li>
+                    <a href = "${pageContext.request.contextPath }/recipe?id=${recipe.id }" >
+                        <c:out value = "${recipe.id }" />
+                    </a>
+                    :<c:out value = "${recipe.title }" ></c:out> &gt;
+
+                </li>
+            </c:forEach>
+        </ul>
+
+        <div id = "pagination" >
+            (全 ${recipes_count } 件)<br/>
+
+            <c:forEach var = "i" begin = "1" end = "${((recipes_count - 1) / 10 ) + 1 }" step = "1" >
+                <c:choose>
+                    <c:when test = "${i == page }" >
+                        <c:out value = "${i }" /> &nbsp;
+                    </c:when>
+
+                    <c:otherwise>
+                        <a href = "${pageContext.request.contextPath }/index?page=${i }" >
+                        <c:out value = "${i }" /></a> &nbsp;
+
+                    </c:otherwise>
+                </c:choose>
+            </c:forEach>
+        </div>
+
     </c:param>
 </c:import>
 
--- src/main/java/controllors/IndexServlet.java
+++ src/main/java/controllors/IndexServlet.java
@@ -1,13 +1,18 @@
 package controllors;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Recipe;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class IndexServlet
@@ -28,6 +33,26 @@ public class IndexServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        EntityManager em = DBUtil.createEntityManager();
+
+        int page = 1;
+            try {
+                page = Integer.parseInt(request.getParameter("page"));
+            } catch (NumberFormatException e) {
+
+            }
+
+        List<Recipe> recipes = em.createNamedQuery("getAllRecipes", Recipe.class)
+                .setFirstResult(10 * (page - 1)).setMaxResults(10).getResultList();
+
+        long recipe_count = (long)em.createNamedQuery("getRecipesCount", Long.class).getSingleResult();
+
+        em.close();
+
+        request.setAttribute("recipes", recipes);
+        request.setAttribute("recipe_count", recipe_count);
+        request.setAttribute("page", page);
 
         if (request.getSession().getAttribute("flush") != null) {
             request.setAttribute("flush", request.getSession().getAttribute("flush"));
